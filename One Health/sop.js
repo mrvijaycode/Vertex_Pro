66a63061b1894168e6935440a678ec39c46cfbf6
Vijay Bhaskar <vijaymca@sify.com>
2013-07-16 17:43:33 +0530

    one-health
    working with table height

---
+++ One Health/sop.js
@@ -0,0 +1,114 @@
+
+//***************************************************************
+//* Project Name     : ONE HEALTH
+//* Application name :
+//* Dependencies     :
+//* Limitations      :
+//* Created Date     :15 July 2013
+//* Author           :Vijay Bhaskar C
+//****************************************************************
+
+$(document).ready(function () {
+	alert('working fine sop');
+	$("#divSop").accordion({header : "span"},{heightStyle: "content" });
+	main();
+});
+
+var sops = [];
+
+function main() {
+	try {
+		$().SPServices({
+			operation : "GetListItems", //Method name
+			async : false,
+			//webURL : webUrl,//pass webUrl dynamically
+			listName : "Sops", // List Name
+			//CAMLQueryOptions : "<QueryOptions><IncludeAttachmentUrls>TRUE</IncludeAttachmentUrls></QueryOptions>",
+			CAMLViewFields : '<ViewFields><FieldRef Name="Title" /><FieldRef Name="Former_x0020_Sop" /><FieldRef Name="Corporate_x0020_QA_x0020_Link" /><FieldRef Name="SOP" /><FieldRef Name="Attachment1" /><FieldRef Name="Attachment4" /><FieldRef Name="Attachment3" /><FieldRef Name="Attachment2" /><FieldRef Name="grpName" /></ViewFields>',
+			CAMLQuery : "",
+			//CAMLRowLimit : 1,
+			completefunc : function (xData, Status) {
+
+				//alert(xData.responseText);
+				debugger
+				if (xData.status == 200) {
+					$(xData.responseXML).SPFilterNode("z:row").each(function () {
+
+						//var keyMeasure = $(this).attr("ows_Key_x0020_Measure");
+
+						if ($(this).attr("ows_SOP") != null)
+							var sop = $(this).attr("ows_SOP");
+						else
+							var sop = '';
+
+						if ($(this).attr("ows_ID") != null)
+							var itmid = $(this).attr("ows_ID");
+						else
+							var itmid = '';
+
+						if ($(this).attr("ows_Title") != null)
+							var title = $(this).attr("ows_Title");
+						else
+							var title = '';
+
+						if ($(this).attr("ows_Attachment1") != null)
+							var attachments = $(this).attr("ows_Attachment1");
+						else
+							var attachments = '';
+
+						if ($(this).attr("ows_Former_x0020_Sop") != null)
+							var formersop = $(this).attr("ows_Former_x0020_Sop");
+						else
+							var formersop = '';
+
+						if ($(this).attr("ows_Corporate_x0020_QA_x0020_Link") != null)
+							var corporateqa = $(this).attr("ows_Corporate_x0020_QA_x0020_Link");
+						else
+							var corporateqa = '';
+
+						if ($(this).attr("ows_grpName") != null)
+							var grpName = $(this).attr("ows_grpName");
+						else
+							var grpName = '';
+
+						var new_obj = {
+							"itmid" : itmid,
+							"title" : title,
+							"formersop" : formersop,
+							"corporateqa" : corporateqa,
+							"attachments" : attachments,
+							"sop" : sop,
+							"grpName" : grpName
+						};
+
+						sops.push(new_obj);
+
+					});
+				} else {
+					alert(xData.status);
+				}
+
+			}
+		});
+	} catch (err) {
+		alert(err.message);
+	}
+	buildContents();
+}
+
+function buildContents() {
+	var tblsop = "<div><table>";
+	tblsop += ' <tbody><thead> <tr class="subheader_bg"><th width="60" align="left" valign="middle">SOP #</th><th align="left" valign="middle">Title</th><th width="100" align="left" valign="middle">Attachments</th><th width="100" align="left" valign="middle">Former SOP #</th><th width="120" align="left" valign="middle">Corporate QA Link</th></tr> </thead> ';
+
+	$.each(sops, function (i) {
+		tblsop += '<tr>';
+		tblsop += '<td width="60" align="left" class="grid_bg" valign="middle">' + sops[i].sop + '</td>';
+		tblsop += '<td align="left" class="grid_bg" valign="middle">' + sops[i].title + '</td>';
+		tblsop += '<td width="100" align="left" class="grid_bg" valign="middle">&nbsp;</td>';
+		tblsop += '<td width="100" align="left" class="grid_bg" valign="middle">' + sops[i].formersop + '</td>';
+		tblsop += '<td width="120" align="left" class="grid_bg" valign="middle">' + sops[i].corporateqa + '</td>';
+		tblsop += '</tr>';
+	});
+	tblsop += "</tbody></table></div>";
+	$('#div1').html(tblsop);
+}
---
+++ One Health/sos.aspx
@@ -0,0 +1,92 @@
+﻿<%@ Page Language="C#" masterpagefile="~masterurl/default.master" title="Untitled 1" inherits="Microsoft.SharePoint.WebPartPages.WebPartPage, Microsoft.SharePoint, Version=12.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" meta:progid="SharePoint.WebPartPage.Document" %>
+<asp:Content id="Content1" runat="server" contentplaceholderid="PlaceHolderMain">
+
+<link href="http://vcs.pg.com/sites/HC/OC/tc/drc/SiteAssets/CSS/style.css" rel="stylesheet" type="text/css" />
+
+				
+<script type="text/javascript" src="http://vcs.pg.com/sites/HC/OC/tc/drc/SiteAssets/JS/jquery-1.8.3.min.js"></script>
+<script type="text/javascript" src="http://vcs.pg.com/sites/HC/OC/tc/drc/SiteAssets/JS/jquery.SPServices-0.7.2.min.js"></script>
+<script type="text/javascript" src="http://vcs.pg.com/sites/HC/OC/tc/drc/SiteAssets/JS/jquery-ui.js"></script>
+<script type="text/javascript" src="http://vcs.pg.com/sites/HC/OC/tc/drc/SiteAssets/JS/sop.js"></script>
+				
+<script type="text/javascript">			
+
+	alert('load..');
+
+</script>
+
+<div id="divSop">
+
+<span align="left" class="group1_header_bg" valign="middle" colspan="5">GROUP 1 – QUALITY LEADERSHIP</span>
+
+<div id="div1">
+    <p>
+    Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
+    ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
+    amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
+    odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.
+    </p>
+   </div>
+
+ 
+	<span align="left" class="group2_header_bg" valign="middle" colspan="5">GROUP 2 – QUALITY SYSTEMS</span>
+  <div id="div2">
+    <p>
+    Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
+    purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
+    velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
+    suscipit faucibus urna.
+    </p>
+  </div>
+<span align="left" class="group3_header_bg" valign="middle" colspan="5">GROUP 3 – DESIGN &amp; DEVELOPMENT
+</span>
+  <div id="div3">
+    <p>
+    Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
+    purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
+    velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
+    suscipit faucibus urna.
+    </p>
+  </div>
+
+
+<span align="left" class="group4_header_bg" valign="middle" colspan="5">GROUP 4 – FACILITIES &amp; EQUIPMENT
+</span>
+
+  <div id="div4">
+    <p>
+    Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
+    purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
+    velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
+    suscipit faucibus urna.
+    </p>
+  </div>
+
+
+
+<span align="left" class="group5_header_bg" valign="middle" colspan="5">GROUP 5 – PRODUCTION SYSTEMS
+</span>
+
+  <div id="div5">
+    <p>
+    Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
+    purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
+    velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
+    suscipit faucibus urna.
+    </p>
+  </div>
+
+<span colspan="5" align="left" valign="middle" class="group6_header_bg">GROUP 6 – PACKAGING &amp; LABELING SYSTEM</span>
+  <div id="div6">
+  </div>
+<span colspan="5" align="left" valign="middle" class="group7_header_bg">GROUP 7 – MATERIAL SYSTEMS</span>
+<div id="div7">
+  </div>
+<span colspan="5" align="left" valign="middle" class="group8_header_bg">GROUP 8 – LABORATORY CONTROL</span>
+<div id="div8">
+  </div>
+<span colspan="5" align="left" valign="middle" class="group9_header_bg">GROUP 9 – REFERENCE FILES</span>
+<div id="div9">
+  </div>
+</div>					
+</asp:Content>
