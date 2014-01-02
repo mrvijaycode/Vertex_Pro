506e27159561613761d1b7d22a82f49354e98bc8
Vijay Bhaskar <vijaymca@sify.com>
2014-01-02 18:51:20 +0530

    Backbone
    Not working this now

--- backbone/rectangles.js
+++ backbone/rectangles.js
@@ -1,12 +1,78 @@
 (function () {
 	var Rectangle = Backbone.Model.extend({});
 	var RectangleView = Backbone.View.extend({
-		tagName:'div',
-		className:'rectangle',
-		
-		render:function(){
-			this.setDimensions();
-		},
+			tagName : 'div',
+			className : 'rectangle',
+
+			events : {
+				'click' : 'move'
+			},
+
+			render : function () {
+				this.setDimensions();
+				this.setPosition();
+				this.setColor();
+				return this;
+			},
+
+			setDimensions : function () {
+				this.$el.css({
+					width : this.model.get('width') + 'px',
+					height : this.model.get('height') + 'px'
+				});
+			},
+
+			setPosition : function () {
+				var position = this.model.get('position');
+				this.$el.css({
+					left : position.x,
+					top : position.y
+				});
+			},
+
+			setColor : function () {
+				this.$el.css('background-color', this.model.get('color'));
+			},
+
+			move : function () {
+				this.$el.css('left', this.$el.position().left + 10);
+			}
+		});
+
+	var models[
+		new Rectangle({
+			width : 100,
+			height : 60,
+			position : {
+				x : 300,
+				y : 150
+			},
+			color : '#ff0000'
+		}),
+		new Rectangle({
+			width : 200,
+			height : 80,
+			position : {
+				x : 200,
+				y : 50
+			},
+			color : '#00ff00'
+		}),
+		new Rectangle({
+			width : 250,
+			height : 30,
+			position : {
+				x : 600,
+				y : 250
+			},
+			color : '#0000ff'
+		})
+	];
+	
+	_(models).each(function (model) { 
+		$('div#canvas').append(new RectangleView({
+				model : model
+			}).render().el);
 	});
-	}
+	
 })();
